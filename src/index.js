e6f751aa749ca25200869093ed265a9afc267975
Aditi <alonhari@newrelic.com>
2020-04-27 17:13:23 -0700

    Fix the render issue

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
+import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { Store } from "./state";
 import { TodoApp } from "./components/todoapp";
 import "./index.css";
 
 // view layer
-function render() {
-  ReactDOM.render(
-    <Provider store={Store}>
-      <TodoApp />
-    </Provider>,
-    document.getElementById("root")
-  );
-  serviceWorker.unregister();
-}
-render();
+render(
+  <Provider store={Store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById("root")
+);
